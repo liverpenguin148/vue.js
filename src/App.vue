8ee97142cab7ab136a0ac3174a3c99fc1f12c169
takahiro.usui <liverpenguin1408@gmail.com>
2020-08-09 01:36:16 +0900

    コンポーネント

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@
     <router-link to="/">Home</router-link>
     <router-link to="/test">Test</router-link>
     <router-link to="/janken">じゃんけん</router-link>
-    <router-view />
+    <router-link to="/component">コンポーネント</router-link>
+    <!-- 動的に表示する -->
+    <router-link to="/user/1">ユーザー１</router-link>
+    <!-- name: URLのパラメータ query: GET送信で取得したデータ -->
+    <router-link :to="{ name: 'user', params: { id: 1 }, query: { id: 'hoge' }}">ユーザー１</router-link>
+    <!-- 遅延ローディング -->
+    <router-link to="/about">About</router-link>
+    <!-- transition -->
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -22,5 +32,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* .fade-enter-active = transitionの画面遷移後の動き
+   .fade-leave-active = transitionの画面から離れる時の動き（コンポーネント要素が無くなっていく始まり）
+*/
+.fade-enter-active,
+.fade-leave-active {
+  /* ２秒間で、透明度を1にしていく→ 画面を２秒かけて表示させる */
+  transition: opacity 1s;
+}
+/* fade-enter = transitionの画面遷移時の状態
+   fade-leave-to = transitionの画面から離れる前の状態
+*/
+.fade-enter,
+.fade-leave-to {
+  /* 透明度０ → 何も表示されない */
+  opacity: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,64 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
 import Janken from '@/components/Janken'
+import Component from '@/components/Component'
+import User from '@/components/User'
+import FirstChild from '@/components/user/FirstChild'
+import SecondChild from '@/components/user/SecondChild'
 
+/* eslint-disable */ // ← esintのルールを無視できる
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld
-  }, {
-    path: '/test',
-    name: 'Test',
-    component: Test
-  }, {
-    path: '/janken',
-    name: 'Janken',
-    component: Janken
-  }]
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/janken',
+      name: 'Janken',
+      component: Janken
+    },
+    {
+      path: '/component',
+      name: 'Component',
+      component: Component
+    },
+    { // 動的に表示するページを変更する
+      path: '/user/:id',
+      name: 'user',
+      // child-router
+      component: User,
+      children: [{
+          path: 'first',
+          name: 'first',
+          component: FirstChild
+        },
+        {
+          path: 'second',
+          name: 'second',
+          component: SecondChild
+        }
+      ]
+    },
+    {
+      // 予期しないURLが入力された場合に、特定のページへリダイレクトさせる（ワイルドカードとリダイレクト）
+      // 注意!! 上から順にルートが決まるので、一番上にワイルドカードを使用してしまうと、全てがそのパスになってしまう
+      path: '*',
+      redirect: '/'
+    },
+    // 遅延ローディング →　クリックされた場合のみ、そのコンポーネントを表示するようにする
+    {
+      path: '/about',
+      name: 'about',
+      component: () =>
+        import( /* webpackChunkNmae: "about" */ '@/components/About')
+    }
+  ]
 })
